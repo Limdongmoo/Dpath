113a5ecfb8c155bd84bcbc7280229b9c2869cc15
Limjongho <whdgh9873@icloud.com>
2022-05-21 15:46:30 +0900

    spring project

---
+++ src/test/java/com/example/Dpath/DpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
